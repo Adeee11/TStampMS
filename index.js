b198cfe4a13a1701f95859f5cf58ec2bbce8cbea
Adarshdeep Singh <adeee621@gmail.com>
2017-03-28 08:15:30 -0400

    new Port

--- index.js
+++ index.js
@@ -64,6 +64,6 @@ app.use((req, res) => {
 	res.status(404).send("Page not Found. Try another page");
 });
 
-app.listen(18320, function() {
-	console.log('Server running on port 18320')
+app.listen(process.env.PORT || 8080, function() {
+	console.log('Server running on port 8080')
 });
